09b2f1a3c385152139157913beb6999a67dbed96
Christopher Duke <cd.duke@gmail.com>
2016-07-13 17:44:57 -0500

    I know not what I do

--- src/get-hand.js
+++ src/get-hand.js
@@ -1,10 +1,18 @@
 const template = `
-
-
-`;
+<div class="card-container">
+  <div class="card"><img class="img" src="http://placekitten.com/200/300" ref""></div>
+  <div class="value">8</div>
+</div>`;
 
 export default class getHand {
   constructor(data) {
     this.data = data;
+
+    this.element = document.createElement('hand');
+    this.element.innerHTML = template;
+
+    this.selectors = {
+      card: this.element.querySelector('.'),
+      value: this.element.querySelector('.'),
   }
 }
--- src/parse-json.js
+++ src/parse-json.js
@@ -1,1 +1,8 @@
 export default (res) => res.json();
+
+export default function () {
+  fetch ('https://card-proxy.herokuapp.com/decks/new')
+  .then(parseJson).then((data) => {
+  //{console.log(data)};
+  }
+}
