0ac062266ca195160ee88e153af7098e3d639f76
Christopher Duke <cd.duke@gmail.com>
2016-07-14 15:25:37 -0500

    help

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
 'use strict';
-import parseJson from 'parse-json';
 import getOptionsData from 'get-data-options';
-import getHand from 'get-hand';
-
-
 
 export default class App {
   constructor(element) {
     this.element = element;
   }
-  getOptions() {
-    return getOptionsData()
-        .then((data) => {
-          this.data = data;
-        });
+
+  start() {
+    this.getNewDeck()
+      // Update what the user sees after getting the new deck
+      .then(() => {
+        const deck = this.data;
+        // Lets do something three times
+        for (let i = 0; i < 3; i++) {
+          // Looking for a few hands of cards
+          const hand = new Hand();
+
+          this.element.appendChild(li)
+        }
+      });
   }
 
+  getNewDeck() {
+    return getOptionsData()
+      .then((data) => {
+        this.data = data;
+      });
   }
+
+}
--- src/parse-json.js
+++ src/parse-json.js
@@ -1,8 +1,1 @@
 export default (res) => res.json();
-
-export default function () {
-  fetch ('https://card-proxy.herokuapp.com/decks/new')
-  .then(parseJson).then((data) => {
-  //{console.log(data)};
-  }
-}
